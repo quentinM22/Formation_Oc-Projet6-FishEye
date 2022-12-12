70ea0f9bb9001044f660a4c96f2c37577f854b22
quentinM22 <quentin.meunier1@laposte.net>
2022-12-12 12:47:59 +0100

    feat: js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,34 +1,9 @@
 async function getPhotographers() {
-    // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-    // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
     const res = await fetch('../../data/photographers.json')
     const data = res.json()
     console.log(data);
     return data
-    // let photographers = [
-    //     {
-    //         "name": "Ma data test",
-    //         "id": 1,
-    //         "city": "Paris",
-    //         "country": "France",
-    //         "tagline": "Ceci est ma data test",
-    //         "price": 400,
-    //         "portrait": "account.png"
-    //     },
-    //     {
-    //         "name": "Autre data test",
-    //         "id": 2,
-    //         "city": "Londres",
-    //         "country": "UK",
-    //         "tagline": "Ceci est ma data test 2",
-    //         "price": 500,
-    //         "portrait": "account.png"
-    //     },
-    // ]
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    // return ({
-    //     photographers: [...photographers, ...photographers, ...photographers]
-    // })
+   
 }
 
 async function displayData(photographers) {
@@ -42,7 +17,6 @@ async function displayData(photographers) {
 };
 
 async function init() {
-    // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
     displayData(photographers);
 };
