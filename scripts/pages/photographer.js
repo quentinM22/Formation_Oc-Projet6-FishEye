8f56284dee944b1639640cfbb520272621d5ba17
quentinM22 <quentin.meunier1@laposte.net>
2022-12-13 16:37:13 +0100

    feat: fetch

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,9 +1,9 @@
 //Mettre le code JavaScript lié à la page photographer.html
 async function getData() {
-    // const urlWithGitHub = 'https://quentinm22.github.io/Formation_Oc-Json_data/photographers.json'
-    // return await fetch(urlWithGitHub)
-    const url = '../data/photographers.json'
-    return await fetch(url)
+    const urlWithGitHub = 'https://quentinm22.github.io/Formation_Oc-Json_data/photographers.json'
+    return await fetch(urlWithGitHub)
+    // const url = '../data/photographers.json'
+    // return await fetch(url)
     .then(res => res.json())
     .then(data => data)
     .catch(error=> console.log("erreur: " + error))
