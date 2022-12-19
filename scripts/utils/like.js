357d3d2cbe6cd1f121b9ddbaa01a76628ee915e3
quentinM22 <quentin.meunier1@laposte.net>
2022-12-19 11:52:05 +0100

    feat: like

---
+++ scripts/utils/like.js
@@ -0,0 +1,29 @@
+// Gestion Like
+function like(event) {
+    const target = event.currentTarget;
+    const targetHeart = target.querySelector('#targetHeart')
+    let newLike = parseInt(target.textContent)
+    if ( !target.checked ) { //Incrementation
+        target.checked = true
+        targetHeart.className = " fa-solid fa-heart fs3 primary anim_heart"
+        newLike++
+        target.querySelector("#btn_like").textContent = newLike;
+        
+    }else{ //Décrementation
+        target.checked = false
+        targetHeart.className = " fa-regular fa-heart fs3 primary anim_heart"
+        newLike--
+        target.querySelector("#btn_like").textContent = newLike;
+    }
+    updateTotalLikes();
+}
+async function updateTotalLikes() {
+    const likes = document.querySelectorAll("#btn_like");
+    const totalLikesNumber = document.querySelector(".allLikes");
+
+    let totalLikes = 0;
+    likes.forEach( like => totalLikes += parseInt(like.textContent) )
+
+    totalLikesNumber.textContent = totalLikes + " ";
+}
+
