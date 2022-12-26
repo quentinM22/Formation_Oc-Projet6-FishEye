a22750b660455439a582a449c2e1aba99726e54f
quentinM22 <quentin.meunier1@laposte.net>
2022-12-26 16:31:08 +0100

    feat: modalKeydown

--- scripts/utils/modalImg.js
+++ scripts/utils/modalImg.js
@@ -1,51 +1,52 @@
 let IsLightboxKeyListenerActive = false;
-document.addEventListener( 'keydown',
-    function (event) {
-        IsLightboxKeyListenerActive ? handleLightboxKeyDown(event) : undefined;
-    }
+document.addEventListener('keydown',
+  function (event) {
+    IsLightboxKeyListenerActive ? handleLightboxKeyDown(event) : undefined;
+  }
 );
 function handleLightboxKeyDown(event) {
   switch (event.key) {
-      case "ArrowLeft":
-        plusSlides(-1)
-          break;
-      case "ArrowRight":
-        plusSlides(+1)
-          break;
-      case "Escape":
-        closeModal();
-          break;
+    case "ArrowLeft":
+      plusSlides(-1)
+      break;
+    case "ArrowRight":
+      plusSlides(+1)
+      break;
+    case "Escape":
+      closeModal();
+      break;
   }
 }
 
 function openModal() {
-    document.querySelector(".mediaModal").style.display = "block";
-    document.querySelector("main").style.display = "none"
+  document.querySelector(".mediaModal").style.display = "block";
+  document.querySelector("main").style.display = "none"
+  IsLightboxKeyListenerActive = true
+}
+
+function closeModal() {
+  document.querySelector(".mediaModal").style.display = "none";
+  document.querySelector("main").style.display = "block"
+  IsLightboxKeyListenerActive = false;
+}
+
+let slideIndex = null;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n + 1);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  function closeModal() {
-    document.querySelector(".mediaModal").style.display = "none";
-    document.querySelector("main").style.display = "block"
-  }
-  
-  let slideIndex = null;
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n + 1);
-  }
-  
-  function showSlides(n) {
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    IsLightboxKeyListenerActive = true
-    slides[slideIndex - 1].style.display = "block";
-  }
-  
+
+  slides[slideIndex - 1].style.display = "block";
+}
